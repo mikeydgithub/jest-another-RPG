94cc7a600fea21c77532a10e8e777a978c85e320
Michael Diamond <94988620+mikeydgithub@users.noreply.github.com>
2022-02-24 17:49:42 -0500

    created tests and functions for getAttackValue, isAlive, addPotion, usePotion, and reduceHealth.

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -47,7 +47,7 @@ test('checks if player is alive or not', () => {
     // If there is a new player expected them to be alive and equal truthy or if player.health = 0 expected playerisAlive to be falsey, or dead.
 })
 
-test('subracts from players health', () => {
+test('subtracts from players health', () => {
     const player = new Player ('Dave');
     const oldHealth = player.health;
 
@@ -57,5 +57,40 @@ test('subracts from players health', () => {
     
     player.reduceHealth(99999);
     expect(player.health).toBe(0);
-    // here the reducedHealth comes out from 99999
+    // here the reducedHealth comes out from 99999 so that we never go negative
+});
+
+test('get players attack value', () => {
+    const player = new Player ('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+    //settinng players default strength to 10 and expecting attackvalue to always be greater then 5 but less than 15.
+});
+
+test('add a potion to inventory', () => {
+    const player = new Player ('Dave');
+    const oldCount = player.inventory.length;
+    //old count is the current amount of items you have in inventory
+    
+    player.addPotion(new Potion());
+    //player adds a potion which becomes new
+
+    expect(player.inventory.length).toBeGreaterThan(oldCount);
+    //after add potion, the current items in your inventory is expected to be greater than your old inventory.
+    // current 0 potions, adding a new 1. Total = 0 + 1 = 1.
+});
+
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion (), new Potion (), new Potion ()];
+    const oldCount = player.inventory.length;
+    // player.inventory is the array contain length of inventory (0,1,2)
+
+    player.usePotion(1);
+    // player.usePotion is 1
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+    //expect the players inventory length or items carrieed in inventory to be less.
 });
