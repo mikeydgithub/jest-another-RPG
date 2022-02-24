273f47142407ac30b9b9fd46e48eb0e615f1fbed
Michael Diamond <94988620+mikeydgithub@users.noreply.github.com>
2022-02-24 16:38:28 -0500

    created tests for getting players health value, check if player is alive or not, and subtracgint from players health. Wrote functions to go with the tests.

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -31,3 +31,31 @@ test('gets inventory from player or returns false', () => {
     player.inventory = [];
     expect(player.getInventory()).toEqual(false);
 });
+
+test('gets players health value', () => {
+    const player = new Player ('Dave');
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+    // The expect.stringContaining() method is an expect method that we can use to make sure our string includes our player's health.
+});
+
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+    expect(player.isAlive()).toBeTruthy();
+    
+    player.health = 0;
+    expect(player.isAlive()).toBeFalsy();
+    // If there is a new player expected them to be alive and equal truthy or if player.health = 0 expected playerisAlive to be falsey, or dead.
+})
+
+test('subracts from players health', () => {
+    const player = new Player ('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+    expect(player.health).toBe(oldHealth - 5);
+    //oldHealth takes on the current health. with reduce health by 5 the oldhealth is subtracted by 5.
+    
+    player.reduceHealth(99999);
+    expect(player.health).toBe(0);
+    // here the reducedHealth comes out from 99999
+});
